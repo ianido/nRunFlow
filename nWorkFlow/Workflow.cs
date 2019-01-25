c6a5be205843d33b450879e559a3cbfa04467ad7
Israel Garcia <israelito3000@hotmail.com>
2019-01-25 17:34:01 -0500

    Multi target to net4 and .netstandard2.1

--- nWorkFlow/Workflow.cs
+++ nWorkFlow/Workflow.cs
@@ -19,7 +19,7 @@ namespace Workflow
 
     public class TupleList<T1, T2> : List<Tuple<T1, T2>>
     {
-        public new void Add(T1 item, T2 item2)
+        public void Add(T1 item, T2 item2)
         {
             Add(new Tuple<T1, T2>(item, item2));
         }
